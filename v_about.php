dfdcfeae9045ccc88f53a3f64e799d8acacfc3a0
putrifajriah31 <91424150+putrifajriah31@users.noreply.github.com>
2021-10-01 15:03:26 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+ 	<h4>Nama</h4>
+ 	<ul type="disc">
+ 		<li>Nama Depan : Putri</li>
+ 		<li>Nama Belakang : Fajriah</li>
+ 		<li>Nim : 19200278</li>
+ 	</ul>
+ <br>
+ 	<h4>Alamat</h4>
+ 	<ul type="none">
+ 		<li> Jln.Angsana Dalam No.78F</li>
+ 	</ul>
+ 
+ 	<h4>Tempat Lahir</h4>
+ 	<ul type="none">
+ 		<li>Jakarta</li>
+ 	</ul>
+ 	<h4>Olahraga Favorite</h4>
+ 	<ul type="square">
+ 		<li>Volly</li>
+ 		<li>Renang</li>
+ 	</ul>
+</section>
---
+++ v_footer.php
@@ -0,0 +1,6 @@
+		<footer>
+			<a href="http://www.Rental buku.net">Sekian Terima Kasih</a>
+		</footer>
+	</div>
+</body>
+</html>
